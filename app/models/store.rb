93ef33f6dc87463d0ea917c9d15a29b2eb84c4fc
針田幹也 <coat@haridenmikiyas-MacBook-Pro.local>
2025-03-05 22:31:41 +0900

    修正3

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ApplicationRecord
   validates :name, :address, :phone_number, :place_id, presence: true
+  validates :web_site, format: { with: URI::regexp(%w[http https]), message: "must be a valid URL" }
 end
