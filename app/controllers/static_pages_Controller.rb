5e47c8f06d47d5acca39e2d3914a9c741b4e9846
針田　幹也 <Coat.kami@gmail.com>
2025-03-05 23:35:11 +0900

    Revert "店舗一覧の表示"

--- app/controllers/static_pages_Controller.rb
+++ app/controllers/static_pages_Controller.rb
@@ -1,23 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top create_store]
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  skip_before_action :require_login, only: %i[top]
 
-  def top
-    @stores = Store.all
-  end
-
-  def create_store
-    store = Store.new(store_params)
-    if store.save
-      render json: { message: "#{store.name}をStoresテーブルに保存しました" }, status: :created
-    else
-      render json: { errors: store.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def store_params
-    params.require(:store).permit(:name, :address, :phone_number, :web_site, :place_id)
-  end
+  def top; end
 end
--- app/models/store.rb
+++
@@ -1,3 +0,0 @@
-class Store < ApplicationRecord
-  validates :name, :address, :phone_number, :place_id, presence: true
-end
