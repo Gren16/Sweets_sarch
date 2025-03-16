021eb218950ee13295df0a50a1b454a438581e5a
針田幹也 <coat@haridenmikiyas-MacBook-Pro.local>
2025-03-16 21:41:22 +0900

    店舗情報の重複修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_28_121635) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_16_123422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_28_121635) do
     t.string "place_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_stores_on_place_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
