86332f4156c18aa74794f818562b8a9c88be7f0a
針田幹也 <coat@haridenmikiyas-MacBook-Pro.local>
2025-04-13 01:20:35 +0900

    修正3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,14 @@ module ApplicationHelper
       charset: "utf-8",
       description: "sweets_sarchでは、周辺のスイーツ店を表示させ気に入ったお店をブックマークすることができ、ルートを作成することができます。",
       keywords: "菓子,スイーツ,和菓子,ルート",
-      canonical: "sweets-sarch.net",
+      canonical: "https://sweets-sarch.net",
       separator: "|",
       og: {
         site_name: "sweets_sarch",
         title: "現在地周辺のスイーツ店を地図上に表示させるサービス",
         description: "sweets_sarchでは、周辺のスイーツ店を表示させ気に入ったお店をブックマークすることができ、ルートを作成することができます。", # 修正：文字列を使用
         type: "website",
-        url: "sweets-sarch.net",
+        url: "https://sweets-sarch.net",
         image: image_url("ogp.png"),
         locale: "ja-JP"
       },
