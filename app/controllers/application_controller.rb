d2daae17d22f87d7075432fa3dc1533b1742f73d
針田幹也 <coat@haridenmikiyas-MacBook-Pro.local>
2025-04-14 23:44:47 +0900

    lint修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :require_login, except: [:show], if: -> { params[:controller] == 'high_voltage/pages' }
+  before_action :require_login, except: [ :show ], if: -> { params[:controller] == "high_voltage/pages" }
 
   private
 
