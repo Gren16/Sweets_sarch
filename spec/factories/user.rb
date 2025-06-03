aaca581e42a8ec84978cd15f29b481ee47413c85
針田幹也 <coat@haridenmikiyas-MacBook-Pro.local>
2025-06-04 01:46:14 +0900

    修正13

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "らんてくん" }
     sequence(:email) { |n| "runteq_#{n}@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    password { "password123" }
+    password_confirmation { "password123" }
   end
 end
